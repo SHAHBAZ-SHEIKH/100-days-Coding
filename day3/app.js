1621e970d454c126b6cf51087ca37ad295ea0a16
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-05-23 14:10:51 +0500

    Add files via upload

---
+++ day3/app.js
@@ -0,0 +1,19 @@
+//count the value in Strings words as a parameter?
+
+// const countWord  = (word,char) =>{
+//     var wordsplit = word.split("")
+
+//  var totalCount =   wordsplit.reduce((acc,curr) =>{
+//     // console.log("the acc is "+acc)
+//     // console.log("the curr is "+curr)
+//         if(curr === char){
+//             acc++;
+//         }
+//         return acc
+
+//     },0)
+//     return totalCount
+
+// }
+
+// console.log(countWord("shahbaz ahmad","a"))

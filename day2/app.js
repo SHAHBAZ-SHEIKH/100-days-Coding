accab9a192621ee539f0acb8dc02501f5aa95c12
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-05-23 13:41:16 +0500

    Add files via upload

---
+++ day2/app.js
@@ -0,0 +1,25 @@
+//word Conver the first word in capitalized & concatente hashTag?
+
+// var str  = prompt("Enter user name")
+// var newArr;
+// function hashTag(str){
+//     var arr1 = str.split(" ")
+//     newArr =[]
+//     for(var i=0; i<arr1.length; i++){
+//         newArr.push(arr1[i].slice(0,1).toUpperCase() + arr1[i].slice(1))
+        
+//     }
+//     var updateval = newArr.join("")
+//         if(updateval.length >=280){
+//             console.log("lenght should be less than 2800")
+//             return false
+//         }
+//         else{
+//             console.log(`#${updateval}`)
+//         }
+   
+
+// }
+// hashTag(str)
+
+// console.log(`#${newArr.join("")}`)

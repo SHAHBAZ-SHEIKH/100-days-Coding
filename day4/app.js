7ec1a11877ba9abcce1e000f1bd21ffb835e9e65
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-05-24 12:51:54 +0500

    Add files via upload

---
+++ day4/app.js
@@ -0,0 +1,20 @@
+// Write a function called checktriangleType that three parameters representing the length of the side of triangle.the function should return a string indicating the type of triangle "equiletral","Isocelses","scalene"?
+
+
+// function checkTriangletype(a,b,c){
+//     if(a===b && b===c){
+//         console.log("Equiletral")
+        
+//     }
+//     else if(a===b && b !==c){
+//         console.log("isoceles")
+//     }
+//     else if(a !==b & b!==c){
+//         console.log("Scalene")
+//     }
+//     else{
+//         console.log("Not equal ")
+//     }
+// }
+
+// checkTriangletype(2,2,3)

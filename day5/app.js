ee4d5a306de2753ce4923724098b5a86158279e0
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-05-27 15:14:48 +0500

    Add files via upload

---
+++ day5/app.js
@@ -0,0 +1,8 @@
+// write a function to sort an aaray of numbers in an  ascending order?
+
+// const ascendingOrder = (arr) =>{
+//     return arr.sort((a,b) => a-b)
+
+// }
+
+// console.log(ascendingOrder([4,6,7,2,1]))

eccf9ff1fa11e7d7a164b2d02fc13ae2070b49eb
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-05-28 16:32:17 +0500

    Add files via upload

---
+++ day6/app.js
@@ -0,0 +1,19 @@
+// write a function to check wheter the given string is palindrome or not?
+
+// const palindromeWord = (str) =>{
+//     var wordConvert = ''
+//     var flag = false
+//     for(var i=str.length -1; i>=0; i--){
+//         wordConvert +=str[i]
+//         if(wordConvert === str){
+//             flag = true
+//             console.log("it is palindrome")
+//             break
+//         }
+//     }
+//     if(flag === false){
+//         console.log("its is not palindrome")
+//     }
+
+// }
+// console.log((palindromeWord("racecar")))

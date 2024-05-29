c65b6887cc399520425574277b1bfb7e0730875e
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-05-29 13:47:23 +0500

    Add files via upload

---
+++ day7/app.js
@@ -0,0 +1,14 @@
+// write a function findMax that takes an array of number as input and return the maximum number in the array?
+
+// const findMax = (a) =>{
+//     var maxFind = a[0]
+//     for(var i=0; i<a.length; i++){
+//         if(maxFind < a[i]){
+//             maxFind = a[i]
+//             // console.log(maxFind)
+//         }
+       
+//     }
+//     console.log(maxFind)
+// }
+// findMax([1,2,8,4,5])

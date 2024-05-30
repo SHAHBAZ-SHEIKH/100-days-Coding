5dcc5548cdb7622e1ac71440c918b6e8d01cd495
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-05-30 11:56:32 +0500

    Add files via upload

---
+++ day8/app.js
@@ -0,0 +1,14 @@
+//Write a function to Find Factorial
+
+// function findFactorial(n){
+//     var num =1
+//     for(var i=1; i<=n; i++){
+//         num = num * i
+//     }
+//     if(n==1 || n==0){
+//         return 1
+//     }
+//     return num
+    
+// }
+// console.log(findFactorial(5))

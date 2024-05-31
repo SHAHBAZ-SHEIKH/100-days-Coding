e90ba0ec53f0b638afc9e5dee9d8180a62a833cb
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-05-31 19:57:50 +0500

    Add files via upload

---
+++ day9/app.js
@@ -0,0 +1,14 @@
+// function countWord(word,char){
+//     var count =0
+//     var wordBreakArray = word.split("")
+//     for(var i=0; i<wordBreakArray.length; i++ ){
+//         if(wordBreakArray[i].toLowerCase() === char.toLowerCase()){
+//             count++
+
+//         }
+//     }
+//     return count
+
+// }
+// var message = countWord("Shahbaz Aahmad","a")
+// console.log(`The Word is:${message}`)

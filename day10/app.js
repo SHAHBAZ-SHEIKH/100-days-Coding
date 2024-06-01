dc8d04ea8f9046616f7610eabeeaeecc25051622
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-06-01 14:59:44 +0500

    Add files via upload

---
+++ day10/app.js
@@ -0,0 +1,29 @@
+// function calculateAverage(arr){
+//     var sum = 0;
+//     var average ;
+//     for(var i=0; i<arr.length; i++){
+//         sum +=arr[i]
+//         average = sum/arr.length
+//     }
+//     console.log(`The sum of the array is:${sum}`)
+//     return average
+    
+
+// }
+// var result = calculateAverage([1,2,3,4,5])
+// console.log(`The average of array the is: ${result}`)
+
+
+// Reduce Method
+
+// const calculateAverage = (arr) =>{
+//  var result =    arr.reduce((acc,curr) =>{
+//         acc = acc + curr
+//         return acc
+
+//     },0)
+//     return result
+
+// }
+
+// console.log(calculateAverage([2,3,4,5]))

2d17c0003974db7a859586db3cb2c35d6f6a1b44
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-06-03 13:35:21 +0500

    Add files via upload

---
+++ day11/app.js
@@ -0,0 +1,19 @@
+//Write a Function arraysEqual that takes two array arr1 and arr2 as input and return if the arrays are equal (i.e contain the same element in the same order) otherwise false
+
+// const arrayEqual = (arr1,arr2) =>{
+//     // if(arr1.length !== arr2.length){
+//     //     return false
+//     // }
+    
+
+//     for(var i=0; i<arr1.length; i++){
+//         if(arr1[i] !== arr2[i])
+//             return false
+//     }
+//     return true
+
+// }
+
+// const array1 = [1,2,3,4,5]
+// const array2 = [1,2,3,4,5]
+// const array3 = [1,6,3,4]

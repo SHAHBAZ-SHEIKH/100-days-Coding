25ab2ea0a293f5befabd711c652ccdd00148cbce
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-06-04 09:33:09 +0500

    Add files via upload

---
+++ day12/app.js
@@ -0,0 +1,18 @@
+//Write a Function that takes a number as input and return the sum of the digits
+
+
+// const sumOfDigits = (num) =>{
+//     const b = num.split("")
+//     const c = b.map(item => Number(item))
+    
+//     const result = c.reduce((acc,curr) =>{
+//         acc +=curr
+//         return acc
+
+//     },0)
+//     return result
+
+    
+// }
+// const message = sumOfDigits("12345")
+// console.log(`The sum of Digit is :${message}`)

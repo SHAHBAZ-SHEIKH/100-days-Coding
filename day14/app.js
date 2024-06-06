434e302c965665b4c5e09e05c7c58e0c0145a191
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-06-06 11:17:16 +0500

    Add files via upload

---
+++ day14/app.js
@@ -0,0 +1,17 @@
+// let str = "hello WOrd"
+
+// function vowelsCount(str){
+//     var count =0
+//     let vowels = "aeiouAEIOU"
+//     for(var i=0; i<str.length; i++){
+//         if(vowels.indexOf(str[i]) !== -1){
+//             count +=1
+//         }
+//     }
+//     return count
+    
+// }
+
+// var result = vowelsCount(str)
+
+// console.log(`The total no of Vowels in the String is :${result}`)

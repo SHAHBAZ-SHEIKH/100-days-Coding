0a50408ddc6c4de6fe0d7bbbc901c6a21577405b
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-06-10 13:36:00 +0500

    Add files via upload

---
+++ day15/app.js
@@ -0,0 +1,13 @@
+// function isPowerTwo(num){
+//     let flag = false
+//     for(var i=1;  i<num; i++){
+//         if(2** i ===num){
+//             flag = true
+//         }
+        
+//     }
+//     return flag
+    
+// }
+
+// console.log(isPowerTwo(4))

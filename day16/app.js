0cb336d6a57df40dfa50db56e0a20f846fb8b444
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-06-11 11:23:14 +0500

    Add files via upload

---
+++ day16/app.js
@@ -0,0 +1,14 @@
+// function sumOfSquare(arr){
+//     var sum =0
+//     for(var i=0; i<arr.length; i++){
+//         sum = sum + arr[i] * arr[i]
+//     }
+//     return sum
+// }
+// console.log(sumOfSquare([1,2,3]))
+
+// function sumOfSquare(arr){
+//     return arr.reduce((acc,curr) => (acc +=  curr *  curr),0)
+// }
+
+// console.log(sumOfSquare([1,2,3]))

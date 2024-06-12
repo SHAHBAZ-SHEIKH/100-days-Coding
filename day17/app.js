11b3ca75d41d90bb885f08c1578ca222363f600e
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-06-12 09:37:11 +0500

    Add files via upload

---
+++ day17/app.js
@@ -0,0 +1,13 @@
+//Write a function find the minimum Number
+
+// const minNumber = (arr) =>{
+//     var minFind = arr[0]
+//     for(var i=0; i<arr.length; i++){
+//         if(minFind > arr[i]){
+//             minFind = arr[i]
+//         }
+//     }
+//     return minFind
+
+// }
+// console.log(minNumber([6,2,1,4]))

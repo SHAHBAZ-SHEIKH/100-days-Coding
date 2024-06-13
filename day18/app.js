b6a65875f795dde7f53a05419b26225d4bd4cf8f
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-06-13 15:35:00 +0500

    Add files via upload

---
+++ day18/app.js
@@ -0,0 +1,37 @@
+// Write a function to convert The string in CamelCase
+
+// const stringCamelCase = (str) =>{
+
+//     let strArr = str.split(" ")
+//     let newArr =[]
+//     for(let i=0; i<strArr.length; i++){
+//         newArr.push(strArr[i].slice(0,1).toUpperCase() + strArr[i].slice(1))
+        
+//     }
+//     return newArr.join("")
+// }
+
+// console.log(stringCamelCase("hello world"))
+
+
+// const stringCamelCase = (str) =>{
+//     str = str.split(" ")
+//     console.log(str)
+
+//     str = str.map((curr,index) =>{
+//         if(index ===0){
+//             return curr
+//         }
+//         else{
+//             return curr.slice(0,1).toUpperCase() + curr.slice(1)
+//         }
+        
+//     })
+//     return (str.join(""))
+    
+
+
+
+// }
+// console.log(stringCamelCase("hello world shahbaz"))
+

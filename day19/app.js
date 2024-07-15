240418c38ca60965fdd3c213d89c2bf495d7eb99
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-07-15 12:56:48 +0500

    Add files via upload

---
+++ day19/app.js
@@ -0,0 +1,19 @@
+// Write A function to check the string is lower case or upper case 
+
+
+
+
+// const calculateUpperCase = (str) =>{
+//     var str = str.charCodeAt(0)
+//     if(str >= 65 && str<=90){
+//         console.log("Upper Case")
+//     }
+//     else if(str >=97 && str<=122){
+//         console.log("It's Lower Case")
+//     }
+//     else{
+//         console.log("Its is not a character")
+//     }
+
+// }
+// calculateUpperCase("a")

6dba8051ec8b7e5b2f9e3bd4f28124eb90ddd71b
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-07-16 12:49:00 +0500

    Add files via upload

---
+++ day20/app.js
@@ -0,0 +1,39 @@
+// Write a function to check if a given string starts with a specific string
+
+
+// const checkString = (str1,str2) =>{
+//     str1 = str1.split(" ")
+//     var flag = false
+//     str1 = str1.filter((curr) =>{
+//         if(curr === str2){
+//             flag = true
+//             console.log("true")
+
+//         }
+//     })
+    
+//     if(flag === flag){
+//         console.log("false")
+//     }
+
+// }
+
+// (checkString("hello World","hello"))
+
+
+// const checkString = (str1,str2) =>{
+//     var arr1 = str1.split(" ")
+//     var flag = false
+//     for(var i=0; i<arr1.length; i++){
+//         if(arr1[0] === str2){
+//             flag = true
+//             console.log("true")
+//             break
+
+//         }
+//     }
+//     if(flag === false){
+//         console.log("false")
+//     }
+// }
+// checkString("hello world","world")

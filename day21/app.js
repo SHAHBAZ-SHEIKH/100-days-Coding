097fcda4e8e06b4075224b7112e2d438d082a6e9
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-07-18 15:33:54 +0500

    Add files via upload

---
+++ day21/app.js
@@ -0,0 +1,10 @@
+// Write a function to reverse a string
+
+// const reverseString = (str) =>{
+//     let strReverse= ""
+//     for(var i=str.length -1; i>=0; i--){
+//         strReverse +=str[i]
+//     }
+//     console.log(strReverse)
+// }
+// reverseString("SHAHBAZ")

5fdae74e8ff1a9f6feb786c3c07d7f7e588c4c43
Shahbaz Ahmed <111065849+SHAHBAZSHEIKH80@users.noreply.github.com>
2024-07-19 22:16:57 +0500

    Add files via upload

---
+++ day22/app.js
@@ -0,0 +1,16 @@
+// write a function called CalculateMean that takes an array of number as input and return the mean (average) of those number
+
+// const calculateMean = (arr) =>{
+//     let reslut = arr.reduce((acc,curr) =>{
+//         acc +=curr
+//         return acc
+        
+
+//     },0)
+//     return reslut
+    
+
+// }
+// let arr = [1,2,3,4,5]
+// let average = calculateMean(arr)
+// console.log(`Mean of the array is : ${average/arr.length}`)
